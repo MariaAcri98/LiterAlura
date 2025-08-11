fcc0038bbd457292469938a8c918e9f7cf025c17
Maria Soledad Acri <acrimaria2@gmail.com>
2025-08-11 16:21:27 -0600

    agrege el funcionamiento del menu actualice el pom agrege propiedades de recurso.

--- src/main/java/com/literalura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/literalura/LiterAlura/LiterAluraApplication.java
@@ -1,12 +1,25 @@
 package com.literalura.LiterAlura;
 
+import com.literalura.LiterAlura.service.MenuService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	private final MenuService menuService;
+
+	public LiterAluraApplication(MenuService menuService) {
+		this.menuService = menuService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		menuService.mostarMenu();
+	}
 }
